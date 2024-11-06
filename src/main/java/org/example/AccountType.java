6f1bc95dbc15bc1bd2621860e9b262bb8e3c67ac
dima <030103dim@gmail.com>
2024-11-06 11:06:06 +0200

    create

---
+++ src/main/java/org/example/AccountType.java
@@ -0,0 +1,17 @@
+package org.example;
+public class AccountType {
+    private boolean premium;
+
+    public AccountType(boolean premium) {
+        this.premium = premium;
+    }
+
+    public boolean isPremium() {
+        return premium;
+    }
+
+    @Override
+    public String toString() {
+        return premium ? "premium" : "normal";
+    }
+}
